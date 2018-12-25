b0c0e00ba4d558ee22007b8a8d75b0a89352bf6e
Paballo <pdditshego@gmail.com>
2018-12-25 17:41:09 +0200

    Refactoring: Upped min SDk version to 26, added android architecture components, retrofit and dagger

--- app/src/main/java/za/co/moxomo/model/Vacancy.java
+++ app/src/main/java/za/co/moxomo/model/Vacancy.java
@@ -1,121 +1,30 @@
-package za.co.moxomo.model;
+package za.co.moxomo.model
 
-import org.joda.time.DateTime;
-import org.parceler.Parcel;
-import org.parceler.ParcelPropertyConverter;
+import org.joda.time.DateTime
+import org.parceler.Parcel
+import org.parceler.ParcelPropertyConverter
 
-import za.co.moxomo.helpers.JodaDateTimeConverter;
+import za.co.moxomo.helpers.JodaDateTimeConverter
 
 
 @Parcel
-public class Vacancy {
+class Vacancy {
 
 
-    public Long id;
-    public String job_title;
-    public String description;
-    public String company_name;
-    public String location;
-    public String min_qual;
-    public String duties;
+    var id: Long? = null
+    var job_title: String
+    var description: String
+    var company_name: String
+    var location: String
+    var min_qual: String
+    var duties: String
 
-    @ParcelPropertyConverter(JodaDateTimeConverter.class)
-    public DateTime advertDate;
-    @ParcelPropertyConverter(JodaDateTimeConverter.class)
-    public DateTime closingDate;
-    public String website;
-    public String imageUrl;
-
-    public String getCompany_name() {
-        return company_name;
-    }
-
-    public void setCompany_name(String company_name) {
-        this.company_name = company_name;
-    }
-
-    public String getMin_qual() {
-        return min_qual;
-    }
-
-    public void setMin_qual(String min_qual) {
-        this.min_qual = min_qual;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-
-    public String getJob_title() {
-        return job_title;
-    }
-
-    public void setJob_title(String job_title) {
-        this.job_title = job_title;
-    }
-
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getDuties() {
-        return duties;
-    }
-
-    public void setDuties(String duties) {
-        this.duties = duties;
-    }
-
-
-    public DateTime getClosingDate() {
-        return closingDate;
-    }
-
-    public void setClosingDate(DateTime closingDate) {
-        this.closingDate = closingDate;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public void setWebsite(String website) {
-        this.website = website;
-    }
-
-    public DateTime getAdvertDate() {
-        return advertDate;
-    }
-
-    public void setAdvertDate(DateTime advertDate) {
-        this.advertDate = advertDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    @ParcelPropertyConverter(JodaDateTimeConverter::class)
+    var advertDate: DateTime
+    @ParcelPropertyConverter(JodaDateTimeConverter::class)
+    var closingDate: DateTime
+    var website: String
+    var imageUrl: String
 
 
 }
